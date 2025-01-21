7eb1d8a3e89167fd1fae3fa9dd218aa493004eae
Will Binns-Smith <wbinnssmith@gmail.com>
2025-01-21 13:46:42 -0800

    Split entrypoint/route handling into separate dev and prod versions
    Currently, for `handleEntrypoints`, `handlePagesErrorRoute`, `handleRouteType`, etc, both dev and prod use cases are combined into the same functions. This leads to a lot of branching, use-case-specific arguments, etc. for minimal opportunity for shared code.
    
    In a following PR, entrypoint writing for prod will be done through a single napi call to rust, further branching the two versions.
    
    This splits them formally, at the cost of duplicating a handful of lines in each case. Perhaps in the future we can develop a better system for sharing code, but this makes things far clearer and easier to maintain in this moment.
    
    Test Plan: CI

--- packages/next/src/build/index.ts
+++ packages/next/src/build/index.ts
@@ -186,18 +186,7 @@ import {
 import { getStartServerInfo, logStartInfo } from '../server/lib/app-info-log'
 import type { NextEnabledDirectories } from '../server/base-server'
 import { hasCustomExportOutput } from '../export/utils'
-import {
-  getTurbopackJsConfig,
-  handleEntrypoints,
-  type EntryIssuesMap,
-  handleRouteType,
-  handlePagesErrorRoute,
-  formatIssue,
-  isRelevantWarning,
-  isPersistentCachingEnabled,
-} from '../server/dev/turbopack-utils'
-import { TurbopackManifestLoader } from '../server/dev/turbopack/manifest-loader'
-import type { Entrypoints } from '../server/dev/turbopack/types'
+import { TurbopackManifestLoader } from '../shared/lib/turbopack/manifest-loader'
 import { buildCustomRoute } from '../lib/build-custom-route'
 import { createProgress } from './progress'
 import { traceMemoryUsage } from '../lib/memory/trace'
@@ -217,6 +206,19 @@ import {
 } from '../server/lib/utils'
 import { InvariantError } from '../shared/lib/invariant-error'
 import { HTML_LIMITED_BOT_UA_RE_STRING } from '../shared/lib/router/utils/is-bot'
+import {
+  handleEntrypoints,
+  handlePagesErrorRoute,
+  handleRouteType,
+} from './handle-entrypoints'
+import type { Entrypoints } from './swc/types'
+import {
+  formatIssue,
+  getTurbopackJsConfig,
+  isPersistentCachingEnabled,
+  isRelevantWarning,
+  type EntryIssuesMap,
+} from '../shared/lib/turbopack/utils'
 
 type Fallback = null | boolean | string
 
@@ -1488,7 +1490,6 @@ export default async function build(
           currentEntrypoints,
           currentEntryIssues,
           manifestLoader,
-          devRewrites: undefined,
           productionRewrites: customRoutes.rewrites,
           logErrors: false,
         })
@@ -1529,15 +1530,11 @@ export default async function build(
           for (const [page, route] of currentEntrypoints.page) {
             enqueue(() =>
               handleRouteType({
-                dev,
                 page,
-                pathname: page,
                 route,
-
                 currentEntryIssues,
                 entrypoints: currentEntrypoints,
                 manifestLoader,
-                devRewrites: undefined,
                 productionRewrites: customRoutes.rewrites,
                 logErrors: false,
               })
@@ -1549,13 +1546,10 @@ export default async function build(
           enqueue(() =>
             handleRouteType({
               page,
-              dev: false,
-              pathname: normalizeAppPath(page),
               route,
               currentEntryIssues,
               entrypoints: currentEntrypoints,
               manifestLoader,
-              devRewrites: undefined,
               productionRewrites: customRoutes.rewrites,
               logErrors: false,
             })
@@ -1564,11 +1558,9 @@ export default async function build(
 
         enqueue(() =>
           handlePagesErrorRoute({
-            dev: false,
             currentEntryIssues,
             entrypoints: currentEntrypoints,
             manifestLoader,
-            devRewrites: undefined,
             productionRewrites: customRoutes.rewrites,
             logErrors: false,
           })
